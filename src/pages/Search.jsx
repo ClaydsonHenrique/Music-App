93a576bbb2161e727b71370673c0a9e9b24da51b
Claydson Henrique <claydsonhenriquebs@gmail.com>
2023-04-13 17:13:58 -0300

    finalizando requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,41 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      disable: true,
+    };
+  }
+
+  validateInput = ({ target }) => {
+    const { value } = target;
+    if (value.length >= 2) {
+      this.setState(
+        { disable: false },
+      );
+    }
+  };
+
   render() {
+    const {
+      disable,
+    } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          <h1>pagina search</h1>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.validateInput }
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ disable }
+          >
+            Pesquisar
+          </button>
         </div>
       </>
     );
