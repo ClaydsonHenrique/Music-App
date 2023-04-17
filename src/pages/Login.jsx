0ac2c2d279be7ff92fb5a930cef81b4b9271b784
Claydson Henrique <claydsonhenriquebs@gmail.com>
2023-04-17 15:01:14 -0300

    adicinando funcao validateInput ao Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      disable: true,
+    };
+  }
+
+  validateInput = ({ target }) => {
+    const { value } = target;
+    if (value.length >= 3) {
+      this.setState(
+        { disable: false },
+      );
+    }
+  };
+
   render() {
+    const {
+      disable,
+    } = this.state;
     return (
       <div data-testid="page-login">
         <form>
-          <input type="text" data-testid="login-name-input" placeholder="nome" />
+          <input
+            onChange={ this.validateInput }
+            type="text"
+            data-testid="login-name-input"
+            placeholder="nome"
+          />
           <button
             type="submit"
             data-testid="login-submit-button"
-            disabled
+            disabled={ disable }
           >
             Entrar
 
