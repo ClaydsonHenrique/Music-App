727e9cce94a66af011643ca5ccc705800ab998e5
Claydson Henrique <claydsonhenriquebs@gmail.com>
2023-04-19 21:22:20 -0300

    adicionando funcao getuser ao header

---
+++ src/components/Carregando.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Carregando extends React.Component {
+  render() {
+    return (
+      <h1>Carregando...</h1>
+    );
+  }
+}
+export default Carregando;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,76 @@
 import React from 'react';
+import '../styles/Header.css';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Header extends React.Component {
-  render() {
-    return (
-      <header data-testid="header-component">
-        <ul>
-          <li><Link data-testid="link-to-search" to="/search">search</Link></li>
-          <li><Link data-testid="link-to-favorites" to="/favorites">favoritos</Link></li>
-          <li><Link data-testid="link-to-profile" to="/profile">profile</Link></li>
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+    };
+  }
 
-        </ul>
-      </header>
+  render() {
+    const getname = async () => {
+      const resolve = Promise.resolve(getUser());
+      const data = resolve.then((v) => {
+        this.setState(
+          { name: v.name },
+        );
+      });
+      return data;
+    };
+    const {
+      name,
+    } = this.state;
+    return (
+      <div>
+        {
+          !getname()
+            ? <Carregando />
+            : (
+              <header data-testid="header-component">
+                <ul>
+                  <li>
+                    <Link
+                      className="link"
+                      data-testid="link-to-search"
+                      to="/search"
+                    >
+                      search
+                    </Link>
+
+                  </li>
+                  <li>
+                    <Link
+                      className="link"
+                      data-testid="link-to-favorites"
+                      to="/favorites"
+                    >
+                      favoritos
+                    </Link>
+
+                  </li>
+                  <li>
+                    <Link
+                      className="link"
+                      data-testid="link-to-profile"
+                      to="/profile"
+                    >
+                      profile
+
+                    </Link>
+
+                  </li>
+                </ul>
+
+                <p data-testid="header-user-name">{name}</p>
+
+              </header>)
+        }
+      </div>
     );
   }
 }
